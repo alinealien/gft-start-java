baa876ce7542aa11b45fa59ab694b94e2a15344a
alinealien <alinebdantunes@gmail.com>
2022-02-08 18:32:17 -0300

    Atualizando aula de Introdução ao Ecossistema e Documentação Java

--- src/com/dio/MyFirstProgram.java
+++ src/com/dio/MyFirstProgram.java
@@ -4,8 +4,8 @@ import com.dio.base.Order;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        Order order = new Order( "coder1234");
+        final Order order = new Order( "coder1234", totalValue: 0.99);
 
-        System.out.println(order);
+        //System.out.println(order);
     }
 }
--- src/com/dio/base/Order.java
+++ src/com/dio/base/Order.java
@@ -1,8 +1,29 @@
 package com.dio.base;
+import java.math.BigDecimal;
 
 public class Order {
 
     private final String code;
+    private final double totalValue;
+
+    public Order(String code, double totalValue) {
+        this.code = code;
+        this.totalValue = totalValue;
+
+    }
+    public double calcularFee(){
+        if (totalValue > 100) {
+
+        } else {
+            return totalValue * 0.99;
+    }
+
+}
+
+
+
+//Aula: Executando um programa Java
+    /*private final String code;
 
     public Order(String code) {
         this.code = code;
@@ -11,6 +32,5 @@ public class Order {
     public String toString() {
         return "Order= {" +
                 "code='" + code + "'" +
-                "}";
-    }
-}
+                "}";*/
+
