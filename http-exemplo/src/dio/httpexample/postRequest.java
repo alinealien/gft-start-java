b855aae013123c5ad65ff044eb23ac5470790a3f
alinealien <alinebdantunes@gmail.com>
2022-03-01 23:40:04 -0300

    Atualizando aulas: Spring Boot e Padrões de Projetos

--- http-exemplo/src/dio/httpexample/postRequest.java
+++ http-exemplo/src/dio/httpexample/postRequest.java
@@ -1,4 +1,34 @@
 package dio.httpexample;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 public class postRequest {
+
+    public static final String URL_POST = "https://httpbin.org/forms/post";
+
+    public static void main (String[] Args) throws IOException, InterruptedException {
+
+        // Cliente HTTP
+        HttpClient client = HttpClient.newHttpClient();
+
+        // Criar requisição
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .timeout(Duration.ofSeconds(10))
+                .uri(URI.create(URL_POST))
+                .build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+
+
+    }
+
 }
